21068c673cfaa9f3b6ac2930e921b50da5b42279
JAYED BIN ZAKIR <100064883+Jayed0@users.noreply.github.com>
2024-05-26 17:02:30 -0600

    Add files via upload

---
+++ database/migrations/2021_03_02_150841_create_units_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUnitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('units', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->tinyInteger('status')->default('1');
+            $table->integer('created_by')->nullable();
+            $table->string('created_by_name')->nullable();
+            $table->integer('updated_by')->nullable();
+            $table->string('updated_by_name')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('units');
+    }
+}
